52477bb3f6c2560693c1f980af0f43e4a975cae7
ConstLaf <developer.constance@gmail.com>
2020-08-22 04:56:33 -0400

    Fix home view content

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -1,36 +1,45 @@
+import { viewHeader } from '../components/header.js';
+
 export const viewHome = () => {
   const homeContainer = document.getElementById('page-container');
   homeContainer.innerHTML = `
   <div class="home-page">
       <div class="cover-photo column">
-        <img src="../src/assets/Rectanglecover.png" alt="foto portada">
+        <img src="../src/assets/rectangle-cover.png" alt="foto portada">
       </div>
       <div class="home-muapp row">
         <h1>¿Qué es muapp?</h1>
-        <img src="../src/assets/Vectorheart.svg" alt="corazón">
+        <img src="../src/assets/vector-heart.svg" alt="corazón">
         <p>Mujeres Unidas y Activas es una aplicacion mobile informar a las mujeres generando
           conexiones con profesionales y ayudando en su formación y crecimiento integral</p>
       </div>
-      <button class="button" id="info-button">Más información</button>
+      <button class="button-large" id="info-button">Más información</button>
       <div class="recomend-section column">
         <h1>Recomendados para ti</h1>
         <div class="recomend-box row">
-          <img src="../src/assets/Rectangletejido.png" alt="persona tejiendo">
+          <img src="../src/assets/rectangle-tejido.png" alt="persona tejiendo">
           <div class="recomend-detail column">
             <p class="bold">Las técnicas del tejidos</p>
             <p class="light">Gancho agujas, tunecino.</p>
+            <button class="button-medium-outline">Ver video</button>
           </div>
-          <img src="../src/assets/Rectangleemprende.png" alt="persona">
+          <img src="../src/assets/rectangle-emprende.png" alt="persona">
           <div class="recomend-detail column">
-            <p class="bold">Las técnicas del tejidos</p>
-            <p class="light">Gancho agujas, tunecino.</p>
+            <p class="bold">¡Emprende ya!</p>
+            <p class="light">Apoyo en redes sociales</p>
+            <button class="button-medium-outline">Ver video</button>
           </div>
+          <img src="../src/assets/rectangle-therapy.png" alt="manos juntas">
+          <p class="bold">Terapia de grupo</p>
+          <p class="light">Mujeres siempre unidas</p>
+          <button class="button-medium-outline">Ver video</button>
         </div>
-        <button class="button" id="category-button">Filtrar por categoría</button>
+        <button class="button-large" id="category-button">Filtrar por categoría</button>
       </div>
     </div>
   `;
 
+  viewHeader();
   return homeContainer;
 };
 viewHome();
