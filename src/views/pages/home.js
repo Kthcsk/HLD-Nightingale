c709973b37125d61df7f4bdcadaff88841dc3dd8
kthcsk <csk.kath@gmail.com>
2020-08-22 12:43:28 -0400

    information complete

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -6,8 +6,8 @@ import { viewBottomMenu } from '../components/bottom-menu.js';
 export const viewHome = () => {
   const homeContainer = document.getElementById('page-container');
   homeContainer.innerHTML = `
-  <div class="home-page">
-      <div class="cover-photo column">
+  <div class="home-page column">
+      <div class="cover-photo">
         <img src="../src/assets/rectangle-cover.png" alt="foto portada">
       </div>
       <div class="home-muapp row">
@@ -18,24 +18,24 @@ export const viewHome = () => {
           conexiones con profesionales y ayudando en su formación y crecimiento integral</p>
       <button class="button-large" id="info-button">Más información</button>
       <div class="recomend-section column">
-        <h1>Recomendados para ti</h1>
+        <h1 class="subtitle-two">Recomendados para ti</h1>
         <div class="recomend-box row">
           <img src="../src/assets/rectangle-tejido.png" alt="persona tejiendo">
           <div class="recomend-detail column">
             <p class="bold">Las técnicas del tejidos</p>
             <p class="light">Gancho agujas, tunecino.</p>
-            <button class="button-medium-outline">Ver video</button>
+            <a class="link">Ver video</a>
           </div>
           <img src="../src/assets/rectangle-emprende.png" alt="persona">
           <div class="recomend-detail column">
             <p class="bold">¡Emprende ya!</p>
             <p class="light">Apoyo en redes sociales</p>
-            <button class="button-medium-outline">Ver video</button>
+            <a class="link">Ver video</a>
           </div>
           <img src="../src/assets/rectangle-therapy.png" alt="manos juntas">
           <p class="bold">Terapia de grupo</p>
           <p class="light">Mujeres siempre unidas</p>
-          <button class="button-medium-outline">Ver video</button>
+          <a class="link">Ver video</a>
         </div>
         <button class="button-large" id="category-button">Filtrar por categoría</button>
       </div>
--- src/views/pages/information.js
+++ src/views/pages/information.js
@@ -7,12 +7,12 @@ export const viewInformation = () => {
   const pageContainer = document.getElementById('page-container');
   pageContainer.innerHTML = `
     <div class="info-page column">
-        <img src="../src/assets/rectangle-escritorio.png" alt="escritorio">
-        <h1>¿Qué es la seccion informativa?</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi. Lorem ipsum dolor sit amet, 
+        <img class="cover-photo" src="../src/assets/rectangle-escritorio.png" alt="escritorio">
+        <h1 class="subtitle">¿Qué es la seccion informativa?</h1>
+        <p class="text-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi. Lorem ipsum dolor sit amet, 
             consectetur adipiscing elit.</p>
-        <button class="button-large" id="info-button">Más información</button>
-        <h1>Categorías</h1>
+        <a class="link margin-fix" id="info-button">Leer más...</a>
+        <h1 class="subtitle-two">Categorías</h1>
         <button class="button-large-outline">Talleres</button>
         <button class="button-large-outline">Servicios de salud</button>
         <button class="button-large-outline">Servicios financieros</button>
