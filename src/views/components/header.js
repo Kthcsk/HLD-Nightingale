0aea6211382c095bf6617567430957ded725b8f7
kthcsk <csk.kath@gmail.com>
2020-08-22 04:21:17 -0400

    header content and style fix

--- src/views/components/header.js
+++ src/views/components/header.js
@@ -1,13 +1,13 @@
 export const viewHeader = () => {
   const headerContainer = document.getElementById('header-container');
   headerContainer.innerHTML = `
-  <div class="row" id="header">
+  <div class="header row" id="header">
     <img class="user-profile" src="../src/assets/user-profile.png" alt="profile">
      <div class="row">
       <p class="name-app bold">mua</p>
       <p class="name-app light">pp</p>
     </div>
-    <img src="../src/assets/search.svg" alt="search">
+    <img class="search" src="../src/assets/search.svg" alt="search">
   </div>`;
 
   return headerContainer;
