db3b87e11c30de23b8cbe4b6e1360db729b34f2f
kthcsk <csk.kath@gmail.com>
2020-08-22 14:06:28 -0400

    fixed home style

--- src/views/components/bottom-menu.js
+++ src/views/components/bottom-menu.js
@@ -3,23 +3,23 @@ export const viewBottomMenu = () => {
   const bottomMenuContainer = document.getElementById('bottom-menu-container');
   bottomMenuContainer.innerHTML = `
   <div class="bottom-menu row align-items" id="bottom-menu">
-    <div class="column">
+    <div class="align-items column">
       <img class="icon info" src="../src/assets/info.svg" alt="info">
       <p class="text-icon">Información</p>
     </div>
-    <div class="column">
+    <div class="align-items column">
       <img class="icon mentor" src="../src/assets/mentor.svg" alt="mentor">
       <p class="text-icon">Mentor</p>
     </div>
-    <div class="column">
+    <div class="align-items column">
       <img class="icon home" src="../src/assets/home.svg" alt="home">
       <p class="text-icon">Inicio</p>
     </div>
-    <div class="column">
+    <div class="align-items column">
       <img class="icon courses" src="../src/assets/courses.svg" alt="courses">
       <p class="text-icon">Cursos</p>
     </div>
-    <div class="column">
+    <div class="align-items column">
       <img class="icon question" src="../src/assets/question.svg" alt="question">
       <p class="text-icon">Ayuda</p>
     </div>
